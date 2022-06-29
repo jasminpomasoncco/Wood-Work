3485e6edfe33540cffacc10a6ea085c8bbee3219
Jasmin <u20201b236@upc.edu.pe>
2022-06-29 11:36:43 -0500

    mejoras

---
+++ assests/js/script.js
@@ -0,0 +1,41 @@
+window.addEventListener("scroll", function(){
+    var header=document.querySelector("header");
+    header.classList.toggle('down',window.scrollY>0);
+})
+
+var menu = document.querySelector('.nav_menu');
+var menuBtn = document.querySelector('.menu-btn');
+var closeBtn = document.querySelector('.close-btn');
+
+menuBtn.addEventListener("click", () => {
+    menu.classList.add('active');
+});
+
+closeBtn.addEventListener("click", () => {
+    menu.classList.remove('active');
+});
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+
+var data = google.visualization.arrayToDataTable([
+['Task', 'Hours per Day'],
+['Enero',     20],
+['Febrero',      35],
+['Marzo',  25],
+['Abril', 10],
+['Mayo',    10]
+
+
+]);
+
+var options = {
+title: 'Carpinteros sin empleo en el 2022'
+};
+
+var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+chart.draw(data, options);
+}
